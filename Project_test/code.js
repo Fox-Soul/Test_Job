7520ea5a1b4a566874df282fc888bb2e0599f409
Akitsune <kuroko1408@gmail.com>
2019-03-21 10:15:43 +0300

    Mod from 21/03

--- Project_test/code.js
+++ Project_test/code.js
@@ -6,3 +6,13 @@ var b = 2;
 
 console.log(c);
 var c = a%b;
+function next(){
+    console.log('I complete this is lesson');
+}
+function kali(lan, callback){
+    
+    console.log('I learn ' + lan);
+    callback();
+}
+
+kali('JavaScript', next);
